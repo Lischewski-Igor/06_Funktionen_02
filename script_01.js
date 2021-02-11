d8b36c00b9e8020559418db4f77c9ab8ae394efd
Igor <igor.lischewski@web.de>
2021-02-11 10:48:15 +0100

    02_Module_grundrechnenarten

--- script_01.js
+++ script_01.js
@@ -10,6 +10,36 @@
 
 
 
+// Modul: Addition a+b | Test:
+//ausgabe(addieren(2,3));
+function addieren(a,b) {
+    return a + b;
+    }
+// Modul: Subtrahieren a-b | Test:
+//ausgabe(subtrahieren(4,3));
+function subtrahieren(a,b) {
+    return a - b; 
+    }
+// Modul: Multiplizieren a*b | Test:
+//ausgabe(multiplizieren(2,3));
+function multiplizieren(a,b) {
+    return a * b;   
+    }
+// Modul: Dividieren a/b | Test:
+ausgabe(dividieren(1,2));
+ausgabe(dividieren(1,-2));
+ausgabe(dividieren(10,0));
+function dividieren(a,b) {
+    if (b != 0) {
+        // b ist nicht 0 / dividieren
+        return a / b;
+    } else {
+        // b ist 0 / warnmeldung ausgeben
+        return "division durch 0 nicht OK";
+    }
+   
+    }
+
 // Modul: Konsolenausgabe | Test:
 //ausgabe("hi");
 function ausgabe(outputStr) {
